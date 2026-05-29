c512b18c4ec8cacc65a3b94e91ffe819a0a2c771
FELIPE DE OLIVEIRA COSTA <43482351805@info.local>
2026-05-29 10:41:53 -0300

    :sparkles: feat:Finalização da parte de transmissão das telas RF

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "WiFiManager.h"
 #include "MQTTManager.h"
 #include "DebugManager.h"
+#include <Preferences.h>
 
 const uint8_t PINO_TX = 7;
 const uint8_t PINO_RX = 6;
@@ -15,11 +16,12 @@ const char TOPICO_COMANDO[] = "senai134/esp32/projetor";
 void tratarMensagemRecebida(const char *, const String &);
 void tratarJsonComando(const String &);
 
-
 TelaProjecaoRF telaRF(PINO_TX, PINO_RX);
+Preferences preferences;
 
 // Insira aqui o endereco capturado no exemplo anterior
 const uint8_t ENDERECO_DA_MINHA_TELA[5] = {0xCD, 0x4E, 0x0A, 0x01, 0x00};
+const uint8_t ENDERECO_DA_MINHA_TELA_ESQUERDA[5] = {0xCD, 0x4B, 0xF6, 0x01, 0x00};
 
 void setup()
 {
@@ -29,10 +31,9 @@ void setup()
   conectarMQTT();
   registrarCallbackMensagem(tratarMensagemRecebida);
   debugInfo("Sistema pronto");
-    Serial.begin(9600);
-    telaRF.begin(&Serial); //o que está entre parênteses é o argumento que está sendo passado para a função
-    telaRF.setInverterSinal(true); // Ajuste conforme seu hardware
-    
+  Serial.begin(9600);
+  telaRF.begin(&Serial);         // o que está entre parênteses é o argumento que está sendo passado para a função
+  telaRF.setInverterSinal(true); // Ajuste conforme seu hardware
 }
 
 void loop()
@@ -40,7 +41,7 @@ void loop()
   garantirWiFiConectado();
   garantirMQTTConectado();
   loopMQTT();
-  telaRF.update(); //Obrigatório 
+  telaRF.update(); // Obrigatório
 }
 void tratarMensagemRecebida(const char *topico, const String &mensagem)
 {
@@ -66,25 +67,35 @@ void tratarJsonComando(const String &mensagem)
 
   String comando = "";
 
-  if(doc["comando"].is<String>()) comando = doc["comando"].as<String>();
+  if (doc["comando"].is<String>())
+    comando = doc["comando"].as<String>();
 
-  if (comando == "subir")
+  if (comando == "subir lado direito")
   {
     telaRF.enviarCima(ENDERECO_DA_MINHA_TELA);
   }
-  else if (comando == "descer")
+  else if (comando == "descer lado direito")
   {
     telaRF.enviarBaixo(ENDERECO_DA_MINHA_TELA);
   }
-  else if (comando == "parar")
+  else if (comando == "parar lado direito")
   {
     telaRF.enviarParar(ENDERECO_DA_MINHA_TELA);
+  }
+   else if (comando == "subir lado esquerdo")
+  {
+    telaRF.enviarCima(ENDERECO_DA_MINHA_TELA_ESQUERDA);
+  }
+  else if (comando == "descer lado esquerdo")
+  {
+    telaRF.enviarBaixo(ENDERECO_DA_MINHA_TELA_ESQUERDA);
+  }
+  else if (comando == "parar lado esquerdo")
+  {
+    telaRF.enviarParar(ENDERECO_DA_MINHA_TELA_ESQUERDA);
   }
   else
   {
     debugErro("Comando inválido");
   }
-
-
-
 }
